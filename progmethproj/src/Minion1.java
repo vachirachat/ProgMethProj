c9d9337c7b3da98b3cf143245a7bf6b591d988d0
Isaree Jirapogchapone <iceisaree1@gmail.com>
2018-11-13 11:43:13 +0700

    fixxxx

--- progmethproj/src/Minion1.java
+++ progmethproj/src/Minion1.java
@@ -1,18 +1,16 @@
 
 public class Minion1 extends Monster {
-<<<<<<< HEAD
-	halooooooo;
+
 	public void newFunction() {
 		
 	}
-||||||| merged common ancestors
-	halooooooo;
-=======
+
 
 	public Minion1(double hp, int attack, String name) {
 		super(hp, attack, name);
 		// TODO Auto-generated constructor stub
 	}
-	
->>>>>>> d0722b5c31be43cb3039d30e3eb822502589ae77
+	public void special() {
+		
+	}
 }
