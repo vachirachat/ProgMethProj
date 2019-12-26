3907cb06f857a2bac2b1938c6044cfee65ad5ebd
vachirachat <vachirachat@outlook.com>
2019-12-26 14:02:28 +0700

    try fix repo

--- progmethproj/src/Minion1.java
+++ progmethproj/src/Minion1.java
@@ -1,16 +1,18 @@
 
 public class Minion1 extends Monster {
-
+<<<<<<< HEAD
+	halooooooo;
 	public void newFunction() {
 		
 	}
-
+||||||| merged common ancestors
+	halooooooo;
+=======
 
 	public Minion1(double hp, int attack, String name) {
 		super(hp, attack, name);
 		// TODO Auto-generated constructor stub
 	}
-	public void special() {
-		
-	}
+	
+>>>>>>> d0722b5c31be43cb3039d30e3eb822502589ae77
 }
